e65f58d05d9a8cd461163506d4082705ee2c747d
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-02-27 16:17:02 -0300

    inicializar tabuleiro

--- src/main/kotlin/View/TelaPrincipal.kt
+++ src/main/kotlin/View/TelaPrincipal.kt
@@ -11,4 +11,15 @@ class TelaPrincipal : JFrame() {
 
     private val tabuleiro = Tabuleiro(qtdeLinhas = 16, qtdeColunas = 30, qtdeMinas = 50)
     private val painelTabuleiro = PainelTabuleiro(tabuleiro)
+
+    init {
+        tabuleiro.onEvento(this::mostrarResultado)
+        add(painelTabuleiro)
+
+        setSize(690, 438)
+        setLocationRelativeTo(null)
+        defaultCloseOperation = EXIT_ON_CLOSE
+        title = "Campo Minado"
+        isVisible = true
+    }
 }
