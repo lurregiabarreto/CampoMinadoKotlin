8d71078c3ead804d2db03fd8df7bb6f32494680a
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-02-27 15:28:49 -0300

    criar metodo adicionar vizinho

--- src/main/kotlin/Model/Campo.kt
+++ src/main/kotlin/Model/Campo.kt
@@ -6,3 +6,19 @@ data class Campo(val linha: Int, val coluna: Int) {
 
     private val vizinhos = ArrayList<Campo>()
     private val callbacks = ArrayList<(Campo, CampoEvento) -> Unit>()
+    var marcado: Boolean = false
+    var aberto: Boolean = false
+    var minado: Boolean = false
+
+    // Somente leitura
+    val desmarcado: Boolean get() = !marcado
+    val fechado: Boolean get() = !aberto
+    val seguro: Boolean get() = !minado
+    val objetivoAlcancado: Boolean get() = seguro && aberto || minado && marcado
+    val qtdeVizinhosMinados: Int get() = vizinhos.filter { it.minado }.size
+    val vizinhancaSegura: Boolean
+        get() = vizinhos.map { it.seguro }.reduce { resultado, seguro -> resultado && seguro }
+
+    fun addVizinho(vizinho: Campo) {
+        vizinhos.add(vizinho)
+    }
