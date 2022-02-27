2dea1885e98c82e3892723d2cd36e6d01c011973
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-02-27 15:39:54 -0300

    criar metodo gerar Campos

--- src/main/kotlin/Model/Tabuleiro.kt
+++ src/main/kotlin/Model/Tabuleiro.kt
@@ -5,3 +5,20 @@ enum class TabuleiroEvento { VITORIA, DERROTA }
 class Tabuleiro(val qtdeLinhas: Int, val qtdeColunas: Int, private val qtdeMinas: Int) {
     private val campos = ArrayList<ArrayList<Campo>>()
     private val callbacks = ArrayList<(TabuleiroEvento) -> Unit>()
+
+    init {
+        gerarCampos()
+        associarVizinhos()
+        sortearMinas()
+    }
+    private fun gerarCampos() {
+        for (linha in 0 until qtdeLinhas) {
+            campos.add(ArrayList())
+            for (coluna in 0 until qtdeColunas) {
+                val novoCampo = Campo(linha, coluna)
+                novoCampo.onEvento(this::verificarDerrotaOuVitoria)
+                campos[linha].add(novoCampo)
+            }
+        }
+    }
+}
