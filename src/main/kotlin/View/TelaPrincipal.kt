6a67d38c156710bd8124be6ccb9c7f0c92bced6b
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-02-27 16:16:20 -0300

    Instanciar tabuleiro e painel de tabuleiro

---
+++ src/main/kotlin/View/TelaPrincipal.kt
@@ -0,0 +1,14 @@
+package View
+
+import Model.Tabuleiro
+import javax.swing.JFrame
+
+fun main(args: Array<String>) {
+    TelaPrincipal()
+}
+
+class TelaPrincipal : JFrame() {
+
+    private val tabuleiro = Tabuleiro(qtdeLinhas = 16, qtdeColunas = 30, qtdeMinas = 50)
+    private val painelTabuleiro = PainelTabuleiro(tabuleiro)
+}
